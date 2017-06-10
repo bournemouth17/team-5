308cbea389d7af9b5af387f41489ea98f7b41bfd
egle1986 <eglearge@mac.com>
2017-06-10 03:46:43 +0100

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+<script>
+setInterval (function PlaySound(soundObj) {
+  var sound = document.getElementById(soundObj);
+  sound.Play();
+}, 10000);
+</script>
+
+<embed src="beeping.wav" autostart="false" width="0" height="0" id="sound1"
+enablejavascript="true">
+
