70096bd95dea0668606aa3c1599e91ed939b01ed
kieran0001 <kieran.amrane@hotmail.com>
2017-06-10 06:46:46 +0100

    Update server.py

--- codeforgood/server.py
+++ codeforgood/server.py
@@ -8,32 +8,33 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-# Conscious_big_sick
 @app.route('/bigsick')
 def bigsick():
 	btn1 = open('information/Conscious_big_sick/Conscious_big_sick.txt', 'r+').read()
 	return render_template('bigsick.html', test=btn1)
 
-# Conscious_big_sick
+
 @app.route('/littlesick')
 def littlesick():
 	btn2 = open('information/Conscious_little_sick/Conscious_little_sick.txt', 'r+').read()
 	return render_template('littlesick.html', test1=btn2)
 
 
-# Conscious_big_sick
+
 @app.route('/breathing')
 def breathing():
 	btn3 = open('information/Unconscious_breathing/Unconscious_breathing.txt', 'r+').read()
 	return render_template('breathing.html', test2=btn3)
 
-# Conscious_big_sick
+
 @app.route('/notbreathing')
 def notbreathing():
 	btn4 = open('information/Unconscious_not_breathing/Unconscious_not_breathing.txt', 'r+').read()
 	return render_template('notbreathing.html', test3=btn4)
 
-
+@app.route('/explore)
+def explore():
+	return render_template(explore.html')
+	
 if __name__ == "__main__":
     app.run(debug=True)
-
